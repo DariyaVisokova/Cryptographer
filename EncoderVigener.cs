615816b2014e62b4ae13b0421d93f45f642c26e1
Дарья Максимовна Высокова <337330@niuitmo.ru>
2022-05-02 20:49:12 +0300

    Добавьте файлы проекта.

---
+++ EncoderVigener.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptographer
+{
+    public class EncoderVigener 
+    {
+        public static string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        public const int ruAlphLength = 33;
+
+
+        public static char TakeSymvFromIndex(int index)
+        {
+            char symv = alphabet[index - 1];
+            return symv;
+        }
+        public static int TakeIndexFromSymv(char symv)
+        {
+            int numString = alphabet.IndexOf(symv) + 1;
+            return numString;
+        }
+        public static int EncrChangPos(char text, char key)
+        {
+
+            int indexPos = (TakeIndexFromSymv(text) + TakeIndexFromSymv(key) - 1) % ruAlphLength;
+            if (indexPos == 0)
+            {
+                indexPos = ruAlphLength;
+            }
+            return indexPos;
+        }
+        public static int DecrChangPos(char text, char key)
+        {
+
+            int indexPos = (TakeIndexFromSymv(text) - TakeIndexFromSymv(key) + 67) % ruAlphLength;
+            if (indexPos == 0)
+            {
+                indexPos = ruAlphLength;
+            }
+            return indexPos;
+        }
+
+        public static string Encode(string input, string keyword)
+        {
+            string result = "";
+            int counter = 0;
+            int index = 0;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (alphabet.Contains(input[i]))
+                {
+                    if (TakeIndexFromSymv(input[i]) <= ruAlphLength)
+                    {
+                        index = EncrChangPos(input[i], keyword[counter]);
+                        result += TakeSymvFromIndex(index);
+                    }
+                    else
+                    {
+                        index = EncrChangPos(input[i], keyword[counter]);
+                        index += ruAlphLength;
+                        result += TakeSymvFromIndex(index);
+                    }
+                    counter++;
+                }
+                else
+                {
+                    result += input[i];
+                }
+                if (counter == keyword.Length)
+                {
+                    counter = 0;
+                }
+            }
+            return result;
+        }
+        public static string Decode(string input, string keyword)
+        {
+            string result = "";
+            int count = 0;
+            int index = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (alphabet.Contains(input[i]))
+                {
+                    if (TakeIndexFromSymv(input[i]) <= ruAlphLength)
+                    {
+                        index = DecrChangPos(input[i], keyword[count]);
+                        result += TakeSymvFromIndex(index);
+                    }
+                    else
+                    {
+                        index = DecrChangPos(input[i], keyword[count]);
+                        index += ruAlphLength;
+                        result += TakeSymvFromIndex(index);
+                    }
+                    count++;
+                }
+                else
+                {
+                    result += input[i];
+                }
+                if (count == keyword.Length)
+                {
+                    count = 0;
+                }
+            }
+            return result;
+        }
+
+    }
+    
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cryptographer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static OpenFileDialog openfile;
+        public static bool? _isFile;
+        public static StreamReader sr;
+        public static String keyInputText;
+        public static string textResult;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Open_Click(object sender, RoutedEventArgs e)
+        {
+            openfile = new OpenFileDialog
+            {
+                DefaultExt = ".txt",
+                Filter = "(.txt)|*.txt"
+            };
+            _isFile = openfile.ShowDialog();
+            inputText.Text = openfile.FileName;
+            if (inputText.Text == "")
+            {
+                MessageBox.Show("Файл не выбран. Попробуйте еще раз.");
+            }
+            else
+            {
+                using (StreamReader reader = new StreamReader(inputText.Text, Encoding.UTF8))
+                {
+                    textResult = await reader.ReadToEndAsync();
+                }
+                inputText.Text = textResult;
+            }
+        }
+
+
+        private void Сipher_Click(object sender, RoutedEventArgs e)
+        {
+            if(inputText.Text == null)
+            {
+                MessageBox.Show("То что пусто, зашифрованным быть не может.");
+                return;
+            }
+            if (keyInputText == null)
+            {
+                MessageBox.Show("Без ключа зашифровать нельзя.");
+                return;
+            }
+            textResult = (EncoderVigener.Encode(inputText.Text, keyInputText));
+            outputText.Text = textResult;
+        }
+
+        private void Decipher_Click(object sender, RoutedEventArgs e)
+        {
+            if (inputText.Text == null)
+            {
+                MessageBox.Show("То что пусто, расшифрованным быть не может.");
+                return;
+            }
+            if (keyInputText == null)
+            {
+                MessageBox.Show("Без ключа расшифровать нельзя.");
+                return;
+            }
+            textResult = (EncoderVigener.Decode(inputText.Text, keyInputText));
+            outputText.Text = textResult;
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (textResult == null)
+            {
+                MessageBox.Show("То чего нет, сохраненным быть не может!");
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                StreamWriter file = new StreamWriter(saveFileDialog.FileName, true, Encoding.UTF8);
+                file.WriteLine(textResult);
+                file.Close();
+            }
+            
+        }
+
+        private void KeyValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            keyInputText = KeyValue.Text;
+        }
+
+    }
+}
